aa3a5c56fd80cbe213a47b515ef103d3972ee1cc
ejob12 <ejobjason4@gmail.com>
2025-10-18 17:26:03 +0100

    Update default cluster name to 'demo-2'

--- 6-eks.tf
+++ 6-eks.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 }
 
 variable "cluster_name" {
-  default = "demo"
+  default = "demo-2"
   type = string
   description = "AWS EKS CLuster Name"
   nullable = false
